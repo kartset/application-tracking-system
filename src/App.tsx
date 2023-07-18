18364a2e81f4a361d670773ec7bf6a8223c68a8a
Kartik Setia <kartset10@gmail.com>
2023-07-18 16:16:58 +0530

    application route structure files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { 
+  Flex, Heading, 
+  ListItem, List, Grid, GridItem, Center
+} from '@chakra-ui/react';
 import './App.css';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello, world!
-      </header>
-    </div>
+    <Grid templateColumns='repeat(12, 1fr)' className="App">
+      <GridItem colSpan={3} >
+        <Sidebar />
+      </GridItem>
+      <GridItem colSpan={9} id="detail">
+        <Center><Outlet /></Center>
+      </GridItem>
+    </Grid>
   );
 }
+function Sidebar() {
+    return (
+      <Flex id="sidebar" justifyContent={'flex-start'} flexDirection={'column'}>
+        <Heading as={'h4'} size={'md'}>Side Bar</Heading>
+        <Flex justifyContent={'space-evenly'}>
+          
+        </Flex>
+        <List>
+          <ListItem>
+          </ListItem>
+          <ListItem>
+          </ListItem>
+        </List>
+      </Flex>
+    );
+  }
 
 export default App;
---
+++ src/routes/contact.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Contact() {
+
+  return (
+    <Flex id="contact">    
+    </Flex>
+  );
+}
