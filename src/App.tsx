bd57c1d867c14747876284401cc5365d4fce93df
Kartik Setia <kartset10@gmail.com>
2023-07-25 14:55:17 +0530

    for show sidebar boolean

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
 import { 
-  Flex, Heading, 
-  ListItem, List, Grid, GridItem, Center
+    Flex, Heading, 
+    ListItem, List, Grid, GridItem
 } from '@chakra-ui/react';
 import './App.css';
 import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <Grid templateColumns='repeat(12, 1fr)' className="App">
-      <GridItem colSpan={3} >
-        <Sidebar />
-      </GridItem>
-      <GridItem colSpan={9} id="detail">
-        <Center><Outlet /></Center>
-      </GridItem>
-    </Grid>
-  );
+    let showSidebar = false
+    return (
+        <>
+            <Grid  templateColumns='repeat(12, 1fr)' className="App">
+               { showSidebar ? <GridItem colSpan={3} ><Sidebar /></GridItem> : <></>}
+                <GridItem colSpan={showSidebar ? 9 : 12} id="detail">
+                    <Outlet />
+                </GridItem>
+            </Grid>
+        </>
+    );
 }
 function Sidebar() {
     return (
-      <Flex id="sidebar" justifyContent={'flex-start'} flexDirection={'column'}>
-        <Heading as={'h4'} size={'md'}>Side Bar</Heading>
-        <Flex justifyContent={'space-evenly'}>
-          
+        <Flex id="sidebar" justifyContent={'flex-start'} flexDirection={'column'}>
+            <Heading as={'h4'} size={'md'}>Side Bar</Heading>
+            <Flex justifyContent={'space-evenly'}>
+                    
+            </Flex>
+            <List>
+                <ListItem>
+                </ListItem>
+                <ListItem>
+                </ListItem>
+            </List>
         </Flex>
-        <List>
-          <ListItem>
-          </ListItem>
-          <ListItem>
-          </ListItem>
-        </List>
-      </Flex>
     );
-  }
+}
 
 export default App;
