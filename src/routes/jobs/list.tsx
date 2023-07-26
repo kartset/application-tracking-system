3f2e5c75658e8d906faa32bea83ae5ad704089de
Kartik Setia <kartset10@gmail.com>
2023-07-26 17:59:14 +0530

    for keys in the array

--- src/routes/jobs/list.tsx
+++ src/routes/jobs/list.tsx
@@ -3,7 +3,7 @@ import Card from "../../components/card"
 
 const List = () => {
     let arr = [1,2,3,4,5,6,7,8,9,10]
-    return (<Box>{arr.map(a => {return (<Card />)})}</Box>)
+    return (<Box>{arr.map(a => {return (<Card key={a} />)})}</Box>)
 }
 
 export default List
