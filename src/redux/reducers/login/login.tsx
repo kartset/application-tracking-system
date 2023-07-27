6ccafd277680b79fd9fdc9ef4551f10eb0dfa6e7
Kartik Setia <kartset10@gmail.com>
2023-07-27 19:42:44 +0530

    for login reducer

---
+++ src/redux/reducers/login/login.tsx
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface CounterState {
+    profile: any,
+    user: any,
+}
+
+const initialState: CounterState = {
+  profile: {},
+  user: {}
+}
+
+export const loginSlice = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {
+    setUser: (state, payload:PayloadAction<any>) => {
+        state.user = payload.payload
+    },
+    setProfile: (state, payload:PayloadAction<any>) => {
+        state.profile = payload.payload
+    }
+  },
+  extraReducers: {}
+})
+
+export const { setUser, setProfile } = loginSlice.actions
+
+export default loginSlice.reducer
