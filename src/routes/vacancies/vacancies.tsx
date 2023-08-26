16816e3d906370cba24a48973190b3b1b46cb6de
Kartik Setia <kartset10@gmail.com>
2023-08-27 02:47:45 +0530

    create a vacancy initial commit

--- src/routes/vacancies/vacancies.tsx
+++ src/routes/vacancies/vacancies.tsx
@@ -4,23 +4,49 @@ import {
 import { 
     Box, Button, Grid, GridItem, Heading, FormControl, 
     FormLabel, Input, InputGroup, InputLeftElement, 
-    Select
+    Select, useDisclosure, Modal, ModalContent, 
+    ModalOverlay, ModalHeader, ModalCloseButton, 
+    ModalBody, ModalFooter, useSteps
 } from "@chakra-ui/react"
 import TableWrapper from "../../components/Table"
+import SteppperWrapper from "../../components/Stepper"
 
 
 
 const Vacancies = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     return (<>
         <GridItem mt={3} as={'div'} style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'center'}} rowSpan={2}>
             <Box ml={4} ><Heading as={'h4'} size={'md'} >Vacancies</Heading></Box>
-            <Box mr={4} ><Button rounded={'xl'} variant={'solid'} leftIcon={<AddIcon />} colorScheme='teal' size={'sm'} >Add New</Button></Box>
+            <Box mr={4} ><Button onClick={() => {onOpen()}} rounded={'xl'} variant={'solid'} leftIcon={<AddIcon />} colorScheme='teal' size={'sm'} >Add New</Button></Box>
         </GridItem>
         <GridItem ml={4} rounded={'2xl'} mr={4} mt={3} as={'div'} style={{backgroundColor:'white', boxShadow: '2px 0px 2px rgba(0, 0, 0, 0.2)', display:'grid', alignItems:'center'}} rowSpan={4}>
             <VacanciesSearchBar />
         </GridItem>
         <TableWrapper />
+        <ModalWrapper isOpen={isOpen} onClose={onClose} />
     </>)
+}
+
+const ModalWrapper:React.FC<any> = ({isOpen, onClose}) => {
+    const steps = Array(7).fill({ title: '' })
+    const { activeStep, setActiveStep } = useSteps({index: 0, count: steps.length})
+    return (
+        <Modal size={'6xl'} blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay  backdropFilter='blur(10px) hue-rotate(90deg)' />
+            <ModalContent>
+                <ModalHeader>Add New Vacancy</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <SteppperWrapper steps={steps} activeStep={activeStep} setActiveStep={setActiveStep} />
+                </ModalBody>
+                <ModalFooter>
+                    <Button colorScheme='blue' mr={3} onClick={onClose}>Close</Button>
+                    <Button variant='ghost'>Secondary Action</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
 }
 
 const VacanciesSearchBar = () => {
