edfe7155e20737e7b1adf6396cbf8ac792fccb12
Kartik Setia <kartset10@gmail.com>
2023-11-21 11:39:55 +0530

    vacancies table columns

---
+++ src/routes/vacancies/data.ts
@@ -0,0 +1,161 @@
+export const tableColumns = [
+    {
+        field: 'position',
+        name: 'Position',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: true,
+        tag: false,
+        boolean: false,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+
+    },
+    {
+        field: 'type',
+        name: 'Type',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: false,
+        truncateText: true,
+        width: '100%',
+        render: () => {}
+
+    },
+    {
+        field: 'currentVacancies',
+        name: 'Curr. Vac.',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: false,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+
+    },
+    {
+        field: 'public',
+        name: 'public',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: true,
+        tag: false,
+        boolean: true,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+
+    },
+    // {
+    //     field: 'remote',
+    //     name: 'Remote',
+    //     mobileOptions: {
+    //         render: () => {},
+    //         show: true
+    //     },
+    //     header: false,
+    //     tag: false,
+    //     boolean: true,
+    //     truncateText: false,
+    //     width: '100%',
+    //     render: () => {}
+
+    // },
+    // {
+    //     field: 'posted',
+    //     name: 'Posted',
+    //     mobileOptions: {
+    //         render: () => {},
+    //         show: true
+    //     },
+    //     header: false,
+    //     tag: false,
+    //     boolean: true,
+    //     truncateText: false,
+    //     width: '100%',
+    //     render: () => {}
+    // },
+    {
+        field: 'salary',
+        name: 'Salary',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: true,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+    },
+    {
+        field: 'location',
+        name: 'Location',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: true,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+    },
+    {
+        field: 'experience',
+        name: 'Experience',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: true,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+    },
+    {
+        field: 'equity',
+        name: 'Equity',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: true,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+    },
+    {
+        field: 'actions',
+        name: 'Actions',
+        mobileOptions: {
+            render: () => {},
+            show: true
+        },
+        header: false,
+        tag: false,
+        boolean: true,
+        truncateText: false,
+        width: '100%',
+        render: () => {}
+    },
+]
