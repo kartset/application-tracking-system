ed27f905c4f1cfff12e3a83ef386ae39b6a5d6f0
Kartik Setia <kartset10@gmail.com>
2023-12-09 20:24:43 +0530

    status enum for async requests

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,10 @@
 export const jobExperienceLevels = [
     'Fresher', 'Associate', 'Senior', 'Executive', 'Advisory'
 ]
+
+export enum STATUS  {
+    NOT_STARTED = "NOT_STARTED",
+    PENDING = "PENDING",
+    COMPLETED = "COMPLETED",
+    FAILED = "FAILEDs"
+}
