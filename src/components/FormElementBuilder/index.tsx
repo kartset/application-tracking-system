1a03b416e6c7ea48d333a0349d40ae529f377f88
Kartik Setia <kartset10@gmail.com>
2023-12-15 04:40:39 +0530

    for formElementBuilder updates

--- src/components/FormElementBuilder/index.tsx
+++ src/components/FormElementBuilder/index.tsx
@@ -1,16 +1,18 @@
 
 import { InfoIcon } from '@chakra-ui/icons'
-import { Flex, Input, Checkbox, Select } from '@chakra-ui/react'
+import { Flex, Input, Checkbox, Select, Box, Switch } from '@chakra-ui/react'
 import { Field } from 'formik'
 import React from 'react'
+import SliderWrapper from '../Slider'
 
 
 interface formFieldProps {
     field: string,
-    text: string,
-    subtext: string,
-    placeholder: string,
+    text: string | undefined,
+    subtext: string | undefined,
+    placeholder: string | undefined,
     elementType: string,
+    element?: any,
     type: string,
     options: { value: string, text:string }[] | undefined,
     subElement: {
@@ -23,43 +25,84 @@ interface formFieldProps {
 
 const FormElement:React.FC<{formField:formFieldProps, index:number}> = ({formField, index}) => {
     return (
-        formField.elementType === HTMLInputElement.name && formField.type === 'input' ?
-            <Field name={formField.field}>
-                {({field, form}:any) => {
-                    return ( 
-                        <Flex flexDirection={'column'} flex={1} justifyContent={'space-evenly'} alignItems={'start'}>
-                            <Input autoFocus={index === 0}
-                                {...field} width={'75%'} placeholder="Enter Job Title"
-                                rounded={'lg'} size={'sm'} 
-                            />
-                            { formField.subElement && formField.subElement.type === 'checkbox' ? 
+        formField.elementType === HTMLInputElement.name ?
+            formField.type === 'input' ?
+                <Field name={formField.field}>
+                    {({field, form}:any) => {
+                        return ( 
+                            <Flex flexDirection={'column'} flex={1} justifyContent={'space-evenly'} alignItems={'start'}>
+                                <Input autoFocus={index === 0}
+                                    {...field} width={'75%'} placeholder="Enter Job Title"
+                                    rounded={'lg'} size={'sm'} 
+                                />
+                                { formField.subElement && formField.subElement.type === 'checkbox' ? 
+                                        <Field name={formField.subElement.field} >
+                                            {({field}:any) => {
+                                                return (
+                                                    <Checkbox 
+                                                        {...field} size={'sm'} 
+                                                        defaultChecked
+                                                    >
+                                                        {formField.subElement!.text}
+                                                    </Checkbox>
+                                                )
+                                            }}
+                                        </Field>
+                                    : <></>
+                                }
+                                <Flex 
+                                    display={ (form.touched[formField.field] && form.errors[formField.field]) ? 'flex' : 'none' } 
+                                    flexDirection='row' gap={2} align={'center'} color={'red'} 
+                                >
+                                    <InfoIcon />
+                                    <span>
+                                        {form.errors[formField.field]}
+                                    </span>
+                                </Flex>
+                            </Flex>
+                        )
+                    }}
+                </Field>
+            : formField.type === 'slider' ?
+                <Field name={formField.field}>
+                    {({field, form}:any) => {
+                        return(
+                            <Flex gap={2} flexDirection={'column'} justifyContent={'center'} flex={1}>
+                                <SliderWrapper name={field.name} type="number" />
+                                { formField.subElement && formField.subElement.type === 'checkbox' ? 
                                     <Field name={formField.subElement.field} >
                                         {({field}:any) => {
-                                            return (
-                                                <Checkbox 
-                                                    {...field} size={'sm'} 
-                                                    defaultChecked
-                                                >
-                                                    {formField.subElement!.text}
-                                                </Checkbox>
-                                            )
+                                            return (<Checkbox {...field} size={'sm'} >{formField.subElement!.text}</Checkbox>)
                                         }}
                                     </Field>
-                                : <></>
-                            }
-                            <Flex 
-                                display={ (form.touched[formField.field] && form.errors[formField.field]) ? 'flex' : 'none' } 
-                                flexDirection='row' gap={2} align={'center'} color={'red'} 
-                            >
-                                <InfoIcon />
-                                <span>
-                                    {form.errors[formField.field]}
-                                </span>
+                                :<></>}
+                                <Flex 
+                                    display={ (form.touched[formField.field] && form.errors[formField.field]) ? 'flex' : 'none' } 
+                                    flexDirection='row' gap={2} align={'center'} color={'red'} 
+                                >
+                                    <InfoIcon />
+                                    <span>
+                                        {form.errors[formField.field]}
+                                    </span>
+                                </Flex>
                             </Flex>
-                        </Flex>
-                    )
-                }}
-            </Field>
+                        )
+                    }}
+                </Field> 
+            :   formField.type === 'switch' ? 
+                    <Field name={formField.field}>
+                        {({field, form}:any) => {
+                            return (
+                                <Flex mt={4} gap={4} justifyContent={'end'} alignItems={'end'}>
+                                    <Box fontSize={'20px'} ><b>Immediate Joining</b></Box>
+                                    <Flex justifyContent={'center'} alignItems={'center'}>
+                                        <Switch isChecked={field.value} {...field} id={formField.field} />
+                                    </Flex>
+                                </Flex>
+                            )
+                        }}
+                    </Field>
+                : <></>
         :   formField.elementType === HTMLSelectElement.name ?
             <Field name={formField.field}>
                 {({field, form}:any) => {
@@ -83,7 +126,28 @@ const FormElement:React.FC<{formField:formFieldProps, index:number}> = ({formFie
                     )
                 }}
             </Field>
-        : <></>
+        :   formField.elementType === 'custom' ?
+                <Field name={formField.field}>
+                    {({field, form}:any) => {
+                        return (
+                            <Flex gap={2} flexDirection={'column'} justifyContent={'center'} alignItems={'start'} flex={1} >
+                                <formField.element 
+                                    { ...(formField.type === 'equityRange' ? {field, form} : {name:field.name})} 
+                                />
+                                <Flex 
+                                    display={(form.touched[formField.field] && form.errors[formField.field]) ? 'flex' : 'none' } 
+                                    flexDirection='row' gap={2} align={'center'} color={'red'} 
+                                >
+                                    <InfoIcon />
+                                    <span>
+                                        {form.errors[formField.field]}
+                                    </span>
+                                </Flex>
+                            </Flex>
+                        )
+                    }}
+                </Field>
+            : <></>
     )
 }
 
