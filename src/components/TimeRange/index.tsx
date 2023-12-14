28cf27a020cb18c5af8888417adbe80a14dcdd16
Kartik Setia <kartset10@gmail.com>
2023-12-15 04:43:12 +0530

    for error and flex display updates in timerange element

--- src/components/TimeRange/index.tsx
+++ src/components/TimeRange/index.tsx
@@ -3,20 +3,24 @@ import { useField } from "formik"
 
 const TimeRange:React.FC<any> = (props) => {
     const { name } = props
-    return (<>
-        <SelectTime name={name} property={['startTime', 'startTimeMeridiem']}  />
-        <Text>-</Text>
-        <SelectTime name={name} property={['endTime', 'endTimeMeridiem']} />
-    </>)
+    const [field, meta, {setValue}] = useField(props);
+    return (
+        <Flex flexDirection={'column'} >
+            <Flex flexDirection={'row'} gap={2} >
+                <SelectTime field={field} setValue={setValue} name={name} property={['startTime', 'startTimeMeridiem']}  />
+                <Text>-</Text>
+                <SelectTime field={field} setValue={setValue} name={name} property={['endTime', 'endTimeMeridiem']} />
+            </Flex>
+        </Flex>
+    )
 }
 
 
-export const SelectTime:React.FC<any> = ({property, ...props}) => {
+export const SelectTime:React.FC<any> = ({property, setValue, field, ...props}) => {
     let meridiemOptions = [
         {value:'ante', text:'a.m.'},
         {value:'post', text:'p.m'}
     ]
-    const [field, meta, {setValue}] = useField(props);
     return (<>
         <Flex gap={2} flexDirection={'column'} >
             <Select {...field} value={field.value[property[0]]} onChange={(e) => setValue({...field.value, [property[0]]:e.target.value})} rounded={'lg'} size={'sm'} placeholder='Select'>
@@ -24,7 +28,6 @@ export const SelectTime:React.FC<any> = ({property, ...props}) => {
                     return (<option key={a} value={i}>{a}</option>)
                 })}
             </Select>
-            <Text>{(meta as any).error && (meta as any).error[property[0]] && (meta as any).error[property[0]] }</Text>
         </Flex>
         <Flex gap={2} flexDirection={'column'}>
             <Select {...field} onChange={(e) => setValue({...field.value, [property[1]]:e.target.value})} value={field.value[property[1]]} rounded={'lg'} size={'sm'} placeholder='Select'>
@@ -35,7 +38,6 @@ export const SelectTime:React.FC<any> = ({property, ...props}) => {
 
                 })}
             </Select>
-            <Text>{(meta as any).error && (meta as any).error[property[1]] && (meta as any).error[property[1]]}</Text>
         </Flex>
     </>)
 }
